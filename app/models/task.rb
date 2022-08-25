153c67b51ff4c3963e455548b3276fed1df99edf
Raphael Cruz <raphacruz_90@hotmail.com>
2022-08-24 21:39:22 -0300

    bar progress included

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,17 @@ class Task < ApplicationRecord
     name.capitalize!
     description.capitalize!
   end
+
+  def self.completed
+    Task.where(done: true).count
+  end
+
+  def self.percent_done
+    return 0 if total == 0
+    (100 * completed.to_f / total).round(1)
+  end
+
+  def self.total
+    Task.count
+  end
 end
